4706782365e0fc5d39c68223acd30612e0b0d7b8
Alex Togo <helloalextogo@gmail.com>
2022-05-17 13:51:45 -0400

    first fetch display countries

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let input = "";
+
+    const fetchCountries = (async () => {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        return await response.json();
+    })();
+
+
+
+</script>
+
+<h1>Countries of the world</h1>
+<h2>Hackathon</h2>
+
+<input type="text" placeholder="Search for a country" bind:value={input}/>
+
+{#await fetchCountries}
+    <p>please wait...</p>
+{:then data}
+Input: {input}
+{console.log(fetchCountries)}
+{#each data as country}
+    <p>{country.name.common}</p>
+{/each}
+{:catch error}
+    <p>An error occured!</p>
+{/await}
+
