00880921896c3f58150b1fdbfc722522878023a1
Alex Togo <helloalextogo@gmail.com>
2022-05-17 16:57:32 -0400

    filtering working

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,12 @@
 <script>
     import Country from '$lib/country.svelte'
-
-    let input = "";
-
-    const fetchCountries = (async () => {
-        const response = await fetch('https://restcountries.com/v3.1/all');
-        return await response.json();
-    })();
-
-
-
+    import { filtered, term } from '../store.js'
 </script>
 
-<input type="text" placeholder="Search for a country" bind:value={input}/>
+<input type="text" placeholder="Search for a country" bind:value={$term}/>
 
-{#await fetchCountries}
-    <p>please wait...</p>
-{:then data}
-{#each data as country}
+{$term}
+{#each $filtered as country}
     <Country title={country.name.common}/>
     <a href={`/countries/${country.cca2}`}>Link -> /countries/{country.cca2}</a>
 {/each}
-{:catch error}
-    <p>An error occured!</p>
-{/await}
